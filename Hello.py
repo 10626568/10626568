65ccbad389106523aeaebc38f8e06cec607aa47c
Anirudh <116883902+Anirudh-Chandran@users.noreply.github.com>
2022-12-02 11:43:38 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,13 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")  # URL leading to method
-def hello():  # Name of the method
-    return render_template('index.html')  # indent this line
+def index():  # Name of the method
+    return render_template("index.html")  # indent this line
 
 
 
 if __name__ == "__main__":
-    print("index.html")
     app.run(host='0.0.0.0', port='8080') # indent this line
