f9457f34ca48e76faeb4b627fa0596def1c4724c
Anirudh <116883902+Anirudh-Chandran@users.noreply.github.com>
2022-12-02 23:55:47 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
-app = Flask(__name__, template_folder="templates")
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import FileField
 
+app = Flask(__name__, template_folder="templates")
+Allowed_Extensions = ['jpg','jpeg','png']
+
+
+class myForms(FlaskForm):
+    image = FileField('photo')
 
 @app.route("/")  # URL leading to method
 def index():  # Name of the method
-    return render_template("index.html")  # indent this line
+    form = myForms()
+    return render_template("index.html", form=form)
 
+
+@app.route("/submission", methods=['GET', 'POST'])
+def submission():
+    if request.method == "POST":
+        fname = request.form['fname']
+        # lname = request.form['lname']
+        image = request.files['photo']
+        print(image)
+        return render_template('submission.html')
+    else:
+        return "except"
 
 
 if __name__ == "__main__":
