7246b61c2102aa8d2f34935c2464518e883cad51
Anirudh <116883902+Anirudh-Chandran@users.noreply.github.com>
2022-12-03 00:28:19 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import FileField
+# from flask_uploads import configure_uploads, IMAGES, UploadSet
+# from werkzeug.utils import secure_filename
 
 app = Flask(__name__, template_folder="templates")
-Allowed_Extensions = ['jpg','jpeg','png']
+app.config['UPLOADED_IMAGES_DEST'] = 'uploads/images'
+app.config['SECRET_KEY'] = 'secretkey'
 
 
 class myForms(FlaskForm):
     image = FileField('photo')
+
 
 @app.route("/")  # URL leading to method
 def index():  # Name of the method
@@ -17,11 +21,10 @@ def index():  # Name of the method
 
 @app.route("/submission", methods=['GET', 'POST'])
 def submission():
+
     if request.method == "POST":
         fname = request.form['fname']
         # lname = request.form['lname']
-        image = request.files['photo']
-        print(image)
         return render_template('submission.html')
     else:
         return "except"
@@ -29,3 +32,4 @@ def submission():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080') # indent this line
+
